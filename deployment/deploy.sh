c7f20d0cd6df7bf85e5de4749b7db3e00c49524c
Dusan Milunovic <dusanmilunovic17@gmail.com>
2021-07-02 00:43:36 +0200

    Add deployment folder and another deploy.sh

---
+++ deployment/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+ALL_HEROKU_APPS=$(heroku apps) && export ALL_HEROKU_APPS
+
+case $ALL_HEROKU_APPS in (*"$TERRAFORM_PG_BACKEND"*)
+    echo "EXISTING BACKEND FOUND"
+    ;;
+(*)
+   heroku create $TERRAFORM_PG_BACKEND
+   heroku addons:create heroku-postgresql:hobby-dev --app $TERRAFORM_PG_BACKEND
+   ;;
+esac
+
+cd terraform || exit
+
+rm -rf ./agent_backend/Dockerfile
+
+echo "FROM $BACKEND_IMAGE" >> ./agent_backend/Dockerfile
+cat ./agent_backend/Dockerfile
+
+DATABASE_URL=$(heroku config:get DATABASE_URL --app "$TERRAFORM_PG_BACKEND") && export DATABASE_URL
+terraform init -backend-config="conn_str=$DATABASE_URL"
+terraform apply -auto-approve -var stage=${STAGE} \
+                              -var token_secret=${TOKEN_SECRET} \
+                              
