06272cb51e2d3bf47b110153c26ee43cd5eb72a9
Dusan Milunovic <dusanmilunovic17@gmail.com>
2021-07-02 01:02:07 +0200

    Save file first commit second

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -20,7 +20,7 @@ variable "token_secret" {
 }
 
 ## backend
-resource "heroku_app" "agent_backend" {
+resource "heroku_app" "agent-backend" {
   name = "${var.stage}-agent-backend"
   stack = "container"
   region = "eu"
@@ -35,7 +35,7 @@ resource "heroku_addon" "postgres" {
   plan = "heroku-postgresql:hobby-dev"
 }
 
-resource "heroku_build" "agent_backend-build" {
+resource "heroku_build" "agent-backend-build" {
   app = heroku_app.agent-backend.id
   source {
     path = "agent-backend"
